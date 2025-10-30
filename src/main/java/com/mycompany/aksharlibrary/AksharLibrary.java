fe0f9e61d8a94238a0ae60bfb478f356c21a1f51
Madhura <madhurapanvelkar8@gmail.com>
2025-10-30 23:34:19 +0530

    NGO Project

---
+++ src/main/java/com/mycompany/aksharlibrary/AksharLibrary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aksharlibrary;
+
+/**
+ *
+ * @author Madhura
+ */
+public class AksharLibrary {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
